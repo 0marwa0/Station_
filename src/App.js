13699118c5ab5717f2f2166d7d118cffa501eb95
marwa <marwajawad19@gmail>
2020-12-04 15:34:58 +0300

    fix issue

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function App(props) {
             exact
           />
           {/* <Route path="/bookingdetalis/:id" component={BookingDetalis} exact /> */}
-          <Route path="/bookinginf0/:55" component={test} exact />
+          <Route path="/bookinginfo/:55" component={test} exact />
 
           <Route path="/home" component={Home} exact />
           <Route
